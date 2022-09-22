6d041550c379437f0aecccc64c5dd6813a9adc9c
Jayme Klein <jaymerebula04@gmail.com>
2022-09-22 09:44:40 -0300

    teste mv

--- teste2/t2.py
+++ teste2/t2.py
@@ -1,2 +1,12 @@
-import this
-#2
+# 2
+from teste3.t1 import t1
+
+t1().arquivo_teste1()
+
+
+class valida_import:
+    def __init__(self):
+        print('Valida import')
+
+    def arquivo_teste2(self):
+        print('deu boa')

465663bc13e6492a52bd99ebc1719eb11350df4f
Jayme Klein <jaymerebula04@gmail.com>
2022-10-04 13:32:26 -0300

    WIP

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 import socket
 
-class udp_client:
+
+class UDPClient:
     mcast_group = '10.151.34.113'
     mcast_port = 5001
     mcast_ttl = 2
@@ -9,20 +10,25 @@ class udp_client:
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, mcast_ttl)
     sock.sendto(b'robot', (mcast_group, mcast_port))
 
-    def __init__(self):
-        pass
 
-class tcp_client:
-    ip = '10.151.34.113'
-    port = 5002
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip, port))
-    # Implementar envio da chave pública deste cliente.
-    sock.sendall(b'JOIN')
-    data = sock.recv(2048)
-    print(f'Received {data}')
+class TCPClient:
+    sock = None
 
-    def __init__(self):
-        pass
+    def __init__(self, ip: str = '127.0.0.1', port: int = 5000) -> None:
+        """
+        :param ip: Ip da máquina onde o servidor TCP está sendo executado.
+        :param port: Porta vinculada ao parâmetro anterior do servidor TCP.
+        """
+        self.ip = ip
+        self.port = port
 
-tcp_client()
+    def conexao_inicial(self) -> str:
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.ip, self.port))
+        self.sock.sendall(b'ENVIAR A CHAVE PUBLICA AQUI')
+        # ESTA VARIÁVEL DEVERÁ SER DECRIPTOGRAFADA UTILIZANDO A CHAVE PRIVADA DESTE CLIENTE.
+        data = self.sock.recv(2048)
+        return data.decode()
+
+
+a = TCPClient(ip='10.1.1.156', port=5000)
--- srv.py
+++ srv.py
@@ -5,23 +5,25 @@ import time
 from datetime import datetime
 import struct
 
+
 class server_thread(threading.Thread):
     def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.counter = counter
-    
+
     def run(self):
         print('Starting' + self.name)
 
+
 class udp_server:
     port = 5001
-    ip =  '10.151.34.113'
-    # socket.gethostbyname(socket.gethostname())  # ip da máquina onde este código está rodando.
+    ip = '10.1.1.156'
+    #  socket.gethostbyname(socket.gethostname()) # ip da máquina onde este código está rodando.
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('10.151.34.113', port))
+    sock.bind(('10.1.1.156', port))
     mreq = struct.pack('4sl', socket.inet_aton('224.1.1.1'), socket.INADDR_ANY)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
@@ -33,21 +35,27 @@ class udp_server:
         while True:
             print(self.sock.recv(2048))
 
-class tcp_server:
-    port = 5002
-    ip = '10.151.34.113'
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind((ip, port))
-    sock.listen()
-    conn, addr = sock.accept()
-    with conn:
-        print(f"Conexão inicial realizada por: {addr}.")
 
-        while True:
+class TCPServer:
+    sock = None
+
+    def __init__(self, port: int = 5000) -> None:
+        self.ip = socket.gethostbyname(socket.gethostname())
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.ip, port))
+
+    def listen(self) -> str:
+        self.sock.listen()
+        conn, addr = self.sock.accept()
+        with conn:
+            print(f"Conexão inicial realizada por: {addr}.")
+
             data = conn.recv(2048)
-            if not data:
-                break
-            # Verificar se a chave pública enviada pelo cliente está na lista de chaves previamente cadastradas.
-            conn.sendall(data)
 
-tcp_server()
+            if data:
+
+                # Verificar se a chave pública enviada pelo cliente está na lista de chaves previamente cadastradas.
+                conn.sendall(data)
+                return data.decode()
+
+
